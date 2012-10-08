77bc596f5ead3ecd059bd7e37b55a6f0bc443355
juanpex <jpma55@gmail.com>
2012-10-08 21:23:42 -0200

    add autodiscover feature to example project, fix datepicker css

--- example/test_project/app/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.reports import *
--- example/test_project/urls.py
+++ example/test_project/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from model_report import report
+report.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
