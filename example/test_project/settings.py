33db04dec8336b0f85a30f5b1ad4c2959abe7030
juanpex <jpma55@gmail.com>
2012-10-09 22:52:33 -0200

    settings for heroku

--- example/test_project/settings.py
+++ example/test_project/settings.py
@@ -21,9 +21,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'example.db',                      # Or path to database file if using sqlite3.
-        # 'USER': '',                      # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'model_report',                      # Or path to database file if using sqlite3.
+        'USER': 'jp',                      # Not used with sqlite3.
         # 'PASSWORD': '',                  # Not used with sqlite3.
         # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
