4c9eb0b22fe2131de7364ad75571a1a8f77543b6
juanpex <jpma55@gmail.com>
2012-10-09 22:23:11 -0200

    settings for heroku

--- example/test_project/settings.py
+++ example/test_project/settings.py
@@ -165,7 +165,38 @@ LOGGING = {
 
 
 try:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-except:
-    pass
+    import os
+    import sys
+    import urlparse
+
+    # Register database schemes in URLs.
+    urlparse.uses_netloc.append('postgres')
+    urlparse.uses_netloc.append('mysql')
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print 'Unexpected error:', sys.exc_info()
