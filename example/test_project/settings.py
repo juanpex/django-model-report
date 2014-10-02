df5383e05544cf9fc10420d8a8206ea8ea652dc5
Administrator <jelena.kutalovskaja@gmail.com>
2014-10-02 13:45:53 +0300

    Example app now is using sqlite3 database.

--- example/test_project/settings.py
+++ example/test_project/settings.py
@@ -19,9 +19,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'model_report',                      # Or path to database file if using sqlite3.
-        'USER': 'jp',                      # Not used with sqlite3.
+        # 'USER': '',                      # Not used with sqlite3.
         # 'PASSWORD': '',                  # Not used with sqlite3.
         # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
